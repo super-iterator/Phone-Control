357c86ee0f760a8a798de550a9030547beba9d64
BL <super.iterator@gmail.com>
2015-10-01 11:11:31 +0200

    added pouchdb query, created a factory as a webservice (didn't work in db.query, used  inside query instead)

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', [ 'ionic'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform , $rootScope, $http, webService) {
       $ionicPlatform.ready(function() {
 
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -17,50 +17,103 @@ angular.module('starter', [ 'ionic'])
               StatusBar.styleDefault();
             }
 
+            var db = new PouchDB('callersDB');
+
             //////// phone call trap works anywhere in the code, but must be insdie $ionicPlatform.ready(...)
             PhoneCallTrap.onCall(function(state) {
-            // alert("CHANGE STATE: " + state.state);
+
+                // console.log('logging state: ..');
+                // console.log(Object.keys(state));
+                // console.log(state.state);
+                // console.log(state.number);
 
                 switch (state.state) {
                     case "RINGING":
                         alert("Phone is ringing: " + state.number);
+
+                        try {
+
+                            db.on('error', function (err) { console.log('debug error: ' + err); });
+
+                            // incoming_number = state.number
+
+                            params = {
+                                incoming_number : state.number,
+                                webService      : webService,
+                                http            : $http
+                            }
+
+                            db.query(function  (doc) {
+
+                                console.log('INITIALIZING PouchDB');
+
+                                // Check if we have the incoming number in the database
+                                if(doc.phone == params.incoming_number){
+
+                                    console.log('Found the document: ' + doc._id);
+
+                                    /////////  Couldn't make the factory working inside the query
+                                    ////////   gives me error everytime
+                                    // params.webService.get()
+                                    //     .success(function  (data, status, headers, config) {
+                                    //         console.log('loggging web service: ');
+                                    //         console.log('Got response from server');
+                                    //         console.log(status);
+                                    //         // console.log(response);
+                                    //     })
+                                    //     .error(function  (data, status, headers, config) {
+                                    //         console.log('Error on Webservice');
+                                    //         console.log(status);
+                                    //     })
+
+                                    console.log('Invoking a web service .. ');
+                                    var promise = params.http.get('http://localhost:1337/users')
+
+                                    promise
+                                        .success(function  (data, status, headers, config) {
+                                            console.log('loggging web service: ');
+                                            console.log('Got response from server');
+                                            console.log(status);
+                                            // console.log(response);
+                                        })
+                                        .error(function  (data, status, headers, config) {
+                                            console.log('Error on Webservice');
+                                            console.log(status);
+                                        })
+
+
+                                    }
+                            });
+
+                        } catch(e) {
+                            console.log('PouchDB Error caught: ' + e);
+                        }
+
+
                         break;
                     case "OFFHOOK":
-                        alert("Phone is off-hook");
+                        console.log("Phone is off-hook");
                         break;
 
                     case "IDLE":
-                        alert("Phone is idle");
+                        console.log("Phone is idle");
                         break;
                     }
             });
 
-
-
-
-
       });
 
-      var db = new PouchDB('callersDB');
-
-      db.put( {_id:'father', phone: "1111"}).then( function (r) {
-                                              console.log('A document inserted: ' + Object.keys(r) + 'ok: ' + r.ok + 'rev: ' + r.rev + 'id: ' + r.id);
-                                          }).catch(function (err) {
-                                            console.log('Error on Puch: ' , err);
-                                          });;
-      db.put( {_id:'mom', phone: "2222"}).then( function (r) {
-                                              console.log('A document inserted: ' + r);
-                                          }).catch(function (err) {
-                                            console.log('Error on Puch: ' , err);
-                                          });;
-      db.put( {_id:'lola', phone: "3333"}).then( function (r) {
-                                              console.log('A document inserted: ' + r);
-                                          }).catch(function (err) {
-                                            console.log('Error on Puch: ' , err);
-                                          });
 })
 
-.controller('myController',['$scope','$ionicPopup',function ( $scope , $ionicPopup ) {
+.controller('myController',['$scope','$ionicPopup','webService',function ( $scope , $ionicPopup , webService) {
+
+
+    $scope.data = {
+        showDelete : false,
+        showReorder : false,
+        listCanSwipe : true,
+    };
+
 
 
     var db = new PouchDB('callersDB');
@@ -82,38 +135,135 @@ angular.module('starter', [ 'ionic'])
             });
 
 
-    $scope.shouldShowDelete = false;
-    $scope.shouldShowReorder = false;
-    $scope.listCanSwipe = true
-
-    $scope.$on('getAll',function  (event,items) {
+    $scope.$on('getAll' , function(event,items) {
+        console.log('ITEMS POPULATED TYPE AND VALUE');
+        console.log(items);
         $scope.items = items
     });
 
+    $scope.$on('updateContacts' , function(event,items) {
+        console.log('Contacts update');
+        console.log(items);
+        $scope.items = items
+    });
+
+
+
+
+
+
+    $scope.removeContact = function (item) {
+
+        console.log('Incoming value: ' + item);
+        console.log(Object.keys(item)); //["id", "key", "value", "doc", "$$hashKey"]
+
+        console.log(item.id);
+        console.log(item.key);
+        console.log(item.doc.phone);
+
+        db.get(item.id).then(function  (element) {
+            console.log('Removing .. ' + element);
+            console.log(Object.keys(element));
+            db.remove(element);
+        }).then(function  (result) {
+            console.log('Removed with DB response: ' + result);
+        }).catch(function  (err) {
+            console.error('Error on removing contact: ' + err);
+        })
+
+
+       //     db.remove(entry.doc._id , entry.doc._rev , function  (entry){
+       //         //{"status":404,"name":"not_found","message":"missing"}
+
+
+    }
+
     $scope.addContact = function  () {
-            $ionicPopup.alert({
-                  title: 'Dont eat that!',
+
+            $scope.user_input = {};
+
+            $ionicPopup.show({
+                  title: 'Data Entry',
                   templateUrl: 'inputForm.html',
+                  subTitle: 'Please enter user Data',
+                  scope: $scope,
+
                   buttons: [
                       {
                         text: '<b>Save</b>',
-                        type: 'button-assertive',
+                        type: 'button-positive',
                         onTap: function(e) {
-                          if (!$scope.data.wifi){}
-                        }
+                          if ($scope.user_input){
+                              var user_input = $scope.user_input
+
+                              db.put( {_id:user_input.name , phone: user_input.phone}).then( function (item) {
+                                              console.log('A document inserted. ');
+
+                                              console.log('a new item inserted: ' + item);
+
+                                              // only id and revision are returned from a put/post operation
+                                              // either we manually add the values we want to show in the list
+                                              // or, make a get operation from DB to retreive all the fields
+                                              // we want --> I'll go for the first
+                                              item.doc = {
+                                                            phone : user_input.phone,
+                                                            _id   : item.id,
+                                                         }
+
+                                              // We have to broadcase it inside an apply service,
+                                              // that's because we are inside an async operation
+                                              $scope.$apply(function () {
+                                                  $scope.items.push(item)
+                                                  $scope.$broadcast('updateContacts',$scope.items)
+                                              })
+                                          }).catch(function (err) {
+                                            console.log('Error on Puch: ' , err);
+                                          })
+                          }
+                      } // End of "onTap"
                       },
 
-                      { text: 'Cancel' },
+                      { text: 'Cancel',
+                        type: 'button-assertive'
+                      },
                    ]
             })
             .then(function(res) {
-              console.log('Thank you for not eating my delicious ice cream cone');
+              console.log('Popup window result: ',res);
             });
     };
 
-    $scope.removeContact = function  () {
-
-    };
 
 
+
+}])
+
+
+.factory('webService',['$http', '$q', function ($http, $q) {
+    console.log('Calling a webserive from factory: ');
+
+    req = {
+         method: 'GET',
+         url: 'http://www.google.com',
+         headers: {
+             'Access-Control-Allow-Origin' : '*',
+  			 'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+            //  'Content-Type': 'application/json',
+			//  'Accept': 'application/json'
+         }
+         //data: { test: 'test' }
+        }
+    var myFactory = {}
+    myFactory.get = function  () {
+        // return $http(req)
+        return $http.get('http://www.google.com')
+    }
+    return myFactory
+
+    // return $http(req)
+
+    // console.log('Google Returend ..' + returned.$$state);
+    // console.log('Google Returend ..' + returned.success);
+    // console.log('Google Returend ..' + returned.error);
+    // return returned
 }])
